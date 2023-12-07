472fe73a92ca090af69d0ab7f0b12215f46e3964
Almina Silnovic <116261786+alminasilnovic@users.noreply.github.com>
2023-12-07 18:57:55 +0100

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.VisualBasic;
+
+namespace Stirnreihe.Data;
+
+public class Person(string firstName, string lastName, int height)
+{
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public int Height { get; set; } = height;
+    public override string ToString()
+    {
+        return $"{LastName}, {FirstName} ({Height} cm)";
+    }
+}
+public class Node(Node? next, Person person)
+{
+    public Person Person { get; set; } = person;
+    public Node? Next { get; set; } = next;
+}
+public class LineOfPeople(Node? first)
+{
+    public Node? First { get; set; } = first;
+    public void AddToFront(Person person)
+    {
+        var node = new Node(First, person);
+        First = node;
+    }
+    public void AddSorted(Person person)
+    {
+        var node = new Node(null, person);
+        if (First == null)
+        {
+            First = node;
+        }
+        else
+        {
+            if (First.Person.Height > node.Person.Height)
+            {
+                AddToFront(person);
+            }
+            else
+            {
+                var current = First;
+                while (current.Next != null)
+                {
+                    if (current.Person.Height < node.Person.Height && node.Person.Height < current.Next.Person.Height)
+                    {
+                        node.Next = current.Next;
+                        current.Next = node;
+                        break;
+                    }
+                    else
+                    {
+                        current = current.Next;
+                    }
+                }
+                current.Next = node;
+            }
+        }
+    }
+    public void Clear()
+    {
+        First = null;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Stirnreihe.Data;
+
+Console.WriteLine("Welcome to the Stirnreihe World Record App! What do you want to do?");
+Console.WriteLine("1) Add a person to the line");
+Console.WriteLine("2) Print the line");
+Console.WriteLine("3) Clear the line");
+Console.WriteLine("4) Exit");
+var choice = "";
+var firstName = "";
+var lastName = "";
+var height = 0;
+var person = new Person(firstName, lastName, height);
+var linesOfPeople = new LineOfPeople(null);
+
+do
+{
+    Console.Write("Your choice: ");
+    choice = Console.ReadLine()!;
+    Console.WriteLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.Write("First name: ");
+            firstName = Console.ReadLine()!;
+            Console.Write("Last name: ");
+            lastName = Console.ReadLine()!;
+            Console.Write("Height in cm: ");
+            height = int.Parse(Console.ReadLine()!);
+            person = new Person(firstName, lastName, height);
+            linesOfPeople.AddToFront(person);
+            break;
+        case "1b":
+            Console.Write("First name: ");
+            firstName = Console.ReadLine()!;
+            Console.Write("Last name: ");
+            lastName = Console.ReadLine()!;
+            Console.Write("Height in cm: ");
+            height = int.Parse(Console.ReadLine()!);
+            person = new Person(firstName, lastName, height);
+            linesOfPeople.AddSorted(person);
+            break;
+        case "2":
+            var current = linesOfPeople.First;
+            while (current != null)
+            {
+                Console.WriteLine(current.Person.ToString());
+                current = current.Next;
+            }
+            break;
+        case "3":
+            linesOfPeople.Clear();
+            Console.WriteLine("The line has been cleared.");
+            break;
+        case "4":
+            break;
+        default:
+            Console.WriteLine("Wrong choice");
+            return;
+    }
+    Console.WriteLine();
+}
+while (choice != "4");
